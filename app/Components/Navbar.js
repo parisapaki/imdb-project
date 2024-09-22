fff9164fa57b7fb3424cdc46b981bfe632a21d8b
parisapaki <parisapakii@gmail.com>
2024-09-22 22:19:42 +0330

    add Suspense boundary to Navbar

--- app/Components/Navbar.js
+++ app/Components/Navbar.js
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import NavbarItem from "./NavbarItem";
 
 export default function Navbar() {
   return (
     <div className="bg-amber-100 dark:bg-gray-600 text-lg p-4 flex justify-center">
-      <NavbarItem title="Trending" address="Trending" />
-      <NavbarItem title="TopRated" address="TopRated" />
+      <Suspense fallback={<div>Loading...</div>}>
+        <NavbarItem title="Trending" address="Trending" />
+        <NavbarItem title="TopRated" address="TopRated" />
+      </Suspense>
     </div>
   );
 }
